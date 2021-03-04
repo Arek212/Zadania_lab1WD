8fc7607ac0bfac87c50b9779fd61ec407ac12968
Arek212 <arek.ugn@wp.pl>
2021-03-04 14:30:58 +0100

    Zadania 1-3

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-print("wynik działąnia jest równy a=%(zm)d" %{'zm':12})
-specjalizacja = "informatyka ogólna"
-rok = 1
-
-print("specjalizacja %s, rok: %d" % (specjalizacja, rok))
+#Zadanie 1-------------------------------------------------
+# tekst1 = "Tekst 1"
+# tekst2 = "Tekst 2"
+# int1 = 3
+# int2 = 4
+# float1 = 3.6
+# float2 = 5.5
+# tedziwne1 = 3 + 3j
+# tedziwne2 = 4 + 7j
+# print(tekst1)
+# print(tekst2)
+# print(int1)
+# print(int2)
+# print(float1)
+# print(float2)
+# print(tedziwne1)
+# print(tedziwne2)
+#Zadanie2----------------------------------------------------
+# a = 5
+# b = 3
+# print("Dodawanie :%(zw)d" %{'zw': a + b})
+# print("Odejmowanie :%(zw)d" %{'zw': a - b})
+# print("Mnożenie :%(zw)d" %{'zw': a * b})
+# print("Dzielenie :%(zw)f" %{'zw': a / b})
+#Zadanie3--------------------------------------------
+# a = 5
+# b = 3
+# a += b
+# print(a)
+# a -= b
+# print(a)
+# a /= b
+# print(a)
+# a **= 2
+# print(a)
+# a %= b
+# print(a)
+#Zadanie4----------------------------------------------------
