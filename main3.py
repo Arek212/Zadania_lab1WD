f6b24044fc6b3b5bd2fd12155a517a6836e9634f
Arek212 <arek.ugn@wp.pl>
2021-03-12 14:27:36 +0100

    oke2

---
+++ main3.py
@@ -0,0 +1,15 @@
+import random
+# Zadanie1
+# a = [1-x for x in range(1,10)]
+# print(a)
+# b = [4 ** x for x in range(8)]
+# print(b)
+# c = [x for x in b if x % 2 == 0]
+# print(c)
+
+# Zadanie2
+# a = [random.randrange(1, 100) for x in range(10)]
+# b = [x for x in a if x % 2 == 0]
+# print(b)
+
+# Zadanie3
