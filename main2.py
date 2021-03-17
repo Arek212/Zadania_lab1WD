0b3d050181d009b9564fa93140d1eaa606c9906f
Arek212 <arek.ugn@wp.pl>
2021-03-17 14:13:16 +0100

    komit2

---
+++ main2.py
@@ -0,0 +1,60 @@
+import sys as system
+# Zadanie1
+# sporty = ["boks", "judo", "snowboard", "szachy"]
+# sporty.reverse()
+# sporty.append("kręglarstwo")
+# sporty.append("jeździectwo")
+# print(sporty)
+
+# Zadanie2
+# skroty = {"wyd." : "wydanie", "t." : "tom", "z." : "zeszyt", "nr." : "numer"}
+# print(skroty)
+
+# Zadanie3
+# gry = {"eu4" : "Europa Universalis 4", "lol" : "League of Legends", "pd2" : "PAYDAY 2", "dbd" : "Dead By Daylight"}
+# print(len(gry))
+
+# Zadanie4
+# x = input()
+# print(x.count("a"))
+
+# Zadanie5
+# system.stdout.write("Wpisz 3 liczby: ")
+# a = system.stdin.readline()
+# b = system.stdin.readline()
+# c = system.stdin.readline()
+# a = int(a)
+# b = int(b)
+# c = int(c)
+# print(pow(a, b) + c)
+
+# Zadanie6
+# print("Wpisz 3 liczby: \n")
+# a = input()
+# b = input()
+# c = input()
+# if a<b:
+#     a=b
+# if a<c:
+#     a=c
+# print(a)
+
+# Zadanie7
+# liczby = [1, 2.5, 4, 6.7, 8, 11.333]
+# for x in range(len(liczby)):
+#     liczby[x]=(pow(liczby[x], 2))
+# print(liczby)
+
+# Zadanie8
+# x = []
+# y = 0
+# print("Wpisz 10 liczb: \n")
+# while y < 10:
+#     z = input()
+#     z = int(z)
+#     if z%2==0:
+#         x.append(z)
+#     y+=1
+# print(x)
+
+# Zadanie9
