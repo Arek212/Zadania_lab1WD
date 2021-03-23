c5006b620a741adb264dff29df22ca7a5697ca35
Arek212 <arek.ugn@wp.pl>
2021-03-24 00:39:32 +0100

    komit nocny1

---
+++ main4.py
@@ -0,0 +1,26 @@
+import sys as system
+import random
+# Zadanie1
+# a = []
+# for i in range(10):
+#     a.append(random.randrange(0,30))
+#
+# plik = open("zad1.txt", "w")
+# for i in a:
+#     plik.write(str(i*2) + " ")
+#
+# plik.close()
+
+# Zadanie2
+# a = open("zad1.txt", "r")
+# print(a.readlines())
+
+# Zadanie3
+# with open("Zad3.txt", "w") as a:
+#     for i in range(3):
+#         a.write("To jest %(ab)i linijka tekstu\n" %{'ab': i + 1})
+# b = open("zad3.txt","r")
+# for i in b:
+#     print(i)
+# Zadanie4
+class NaZakupy:
